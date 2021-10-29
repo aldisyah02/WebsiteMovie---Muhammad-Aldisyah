3a5eacd2e83d4f5f0a3dd13155ba114aaa920088
aldisyah02 <aldisyahrahman@gmail.com>
2021-10-29 13:58:35 +0700

    Menambahkan Repositori WebsiteMovie Tugas GDSC

---
+++ conf/info.php
@@ -0,0 +1,13 @@
+<?php
+
+$apikey = "8d9cd44d792a08a877b3fdbec0414238";
+
+$sitename = "GDSC - Muhammad Aldisyah Rahman";
+$tagline = "List Movie";
+$email = "marinuyul2@gmail.com";
+$url = "http://www.gdsc-mar.com";
+
+$imgurl_1 = "http://image.tmdb.org/t/p/w500";
+$imgurl_2 = "http://image.tmdb.org/t/p/w300";
+
+?>
